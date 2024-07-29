b0b6dbf834e758c12cd005ded36566b72ede7ac6
Artur Cadorin <artur.cadorin@outlook.com>
2024-07-28 23:42:04 -0300

    Criação do banco de dados, utilizando EF

--- Startup.cs
+++ Startup.cs
@@ -37,8 +37,9 @@ namespace ProjetoCrud
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjetoCrudContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoCrudContext")));
-        }
+                    options.UseMySql(Configuration.GetConnectionString("ProjetoCrudContext"), builder => 
+                        builder.MigrationsAssembly("ProjetoCrud")));
+        }   
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)

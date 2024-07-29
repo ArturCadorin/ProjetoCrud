dd6f9bdcb397ef076676d87e3da1fc81d68310eb
Artur Cadorin <artur.cadorin@outlook.com>
2024-07-29 01:28:32 -0300

    Outras entidades e mais uma migration

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,33 @@
-﻿namespace ProjetoCrud.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ProjetoCrud.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        // public ICollection<Seller> = coleção generica da classe sellers(vendedores)
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department(){ }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Adcionando um vendedor
+        public void AddSellers(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller =>  seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoCrud.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        // public Department = departamento do seller(vendedor)
+        public Department Department { get; set; }
+        // public ICollection<SalesRecord> = lista de genéricos para registar as vendas do seller(vendedor)
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        // Adcionando uma venda 
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        // Removendo uma venda
+        public void RemoveSales(SalesRecord sr) 
+        { 
+            Sales.Remove(sr); 
+        }
+
+        // Total as vendas em um determinado intervalo de datas
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
